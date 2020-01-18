629659adfe546cf366a103eb58e2529f84a9f2b2
kris-piece <kristina_plotnikova@protonmail.com>
2020-01-18 17:13:10 +0600

    tt

---
+++ src/proverka/InputOutput/swingesy.java
@@ -0,0 +1,8 @@
+package proverka.InputOutput;
+import javax.swing.*;
+
+public class swingesy {
+    public static void main (String[] args){
+        JOptionPane.showMessageDialog(null,"Это достаточно тривиальная программа для демострации работы старейшей библиотеки джавеса");
+    }
+}
